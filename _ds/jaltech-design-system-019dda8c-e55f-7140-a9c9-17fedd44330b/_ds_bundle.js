e373f62c24fee47421416e863a0fc83f643990ef
mercadeojaltech22-prog <mercadeojaltech22@gmail.com>
2026-07-02 16:43:01 -0500

    Add files via upload

---
+++ _ds/jaltech-design-system-019dda8c-e55f-7140-a9c9-17fedd44330b/_ds_bundle.js
@@ -0,0 +1,11 @@
+/* @ds-bundle: {"format":3,"namespace":"JaltechDesignSystem_019dda","components":[],"sourceHashes":{},"inlinedExternals":[],"unexposedExports":[]} */
+
+(() => {
+
+const __ds_ns = (window.JaltechDesignSystem_019dda = window.JaltechDesignSystem_019dda || {});
+
+const __ds_scope = {};
+
+(__ds_ns.__errors = __ds_ns.__errors || []);
+
+})();
